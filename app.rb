40053318fbfd321032aa7978309bbcb270badd25
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-04 22:25:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Tahseen Siddiqui"
+  end
+
+  get '/hometown' do
+    "My hometown is Hyderabad"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is abc"
+  end
 end
